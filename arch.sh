9a1814c680c98605c6d7daf57e8fbd446b462585
Abbas Toof <abbas.toof@gmail.com>
2023-12-14 14:17:19 +0200

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,77 +1,63 @@
 #!/bin/bash
 
 # Partitioning
-gdisk /dev/sda <<EOF
-o
-Y
-n
-1
-
-+1G
-EF00
-n
-2
-
-+183G
-8300
-n
-3
-
-+16G
-8200
-w
-Y
-EOF
+parted /dev/sda mklabel gpt
+parted /dev/sda mkpart primary 1MiB 1GiB
+parted /dev/sda set 1 esp on
+parted /dev/sda mkpart primary 1GiB 184GiB
+parted /dev/sda mkpart primary 184GiB 200GiB
 
 # Format partitions
-mkfs.ext4 /dev/sda1
 mkfs.ext4 /dev/sda2
-mkswap /dev/sda3
-swapon /dev/sda3
+mkfs.ext4 /dev/sda3
+mkswap /dev/sda4
+swapon /dev/sda4
 
-# Mount system and create directories
+# Mount partitions
 mount /dev/sda2 /mnt
-mkdir /mnt/boot /mnt/var /mnt/home
+mkdir -p /mnt/boot
 mount /dev/sda1 /mnt/boot
 
 # Install base system
-pacman -Syy
-pacstrap /mnt base base-devel linux linux-firmware nano dhcpcd net-tools grub
+pacstrap /mnt base linux linux-firmware nano dhcpcd net-tools grub
 
 # Generate fstab
 genfstab -U /mnt >> /mnt/etc/fstab
 
-# Configure base system
+# Chroot into the installed system
 arch-chroot /mnt
 
-# Set locale
+# Set up locale
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 locale-gen
 echo LANG=en_US.UTF-8 > /etc/locale.conf
-export LANG=en_US.UTF-8
 
 # Set time zone
-ln -s /usr/share/zoneinfo/Europe/Helsinki /etc/localtime
+ln -sf /usr/share/zoneinfo/Europe/Helsinki /etc/localtime
 
-# Set hardware clock and hostname
+# Set hardware clock
 hwclock --systohc --utc
-echo arindam-pc > /etc/hostname
+
+# Set hostname
+echo "arindam-pc" > /etc/hostname
+
+# Enable dhcpcd service
 systemctl enable dhcpcd
 
-# Set root password, create user, and add to sudoers
-passwd root
-useradd -m -g users -G wheel -s /bin/bash atoof
+# Set root password
+passwd
+
+# Create a user
+useradd -m -G wheel -s /bin/bash atoof
 passwd atoof
 
-# Edit sudoers file
-echo "atoof ALL=(ALL) ALL" >> /etc/sudoers
-
-# Install GRUB
+# Install and configure GRUB
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
-mkinitcpio -p linux
-exit
 
-# Unmount system and reboot
+# Exit chroot and unmount partitions
+exit
 umount -R /mnt
+
+# Reboot
 reboot
