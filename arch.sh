ae6fafc008bb6114d864fca1c64c4f508e5415b5
Abbas Toof <abbas.toof@gmail.com>
2023-12-14 14:14:50 +0200

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -45,7 +45,7 @@ genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
 
 # Set locale
-nano /etc/locale.gen
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 locale-gen
 echo LANG=en_US.UTF-8 > /etc/locale.conf
 export LANG=en_US.UTF-8
@@ -64,18 +64,14 @@ useradd -m -g users -G wheel -s /bin/bash atoof
 passwd atoof
 
 # Edit sudoers file
-nano /etc/sudoers
-# Add the following lines:
-# root ALL=(ALL) ALL
-# atoof ALL=(ALL) ALL
+echo "atoof ALL=(ALL) ALL" >> /etc/sudoers
 
 # Install GRUB
-grub-install /dev/sda
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 mkinitcpio -p linux
 exit
 
 # Unmount system and reboot
-umount /mnt/boot
-umount /mnt
+umount -R /mnt
 reboot
